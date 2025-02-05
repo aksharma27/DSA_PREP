dfc2a616b97d658856c4ea326328de801337641e
Abhishek <aksharma.27mjm@gmail.com>
2025-02-06 01:47:26 +0530

    odd journey

--- src/DP/GoogleOaODDJOURNEY.java
+++ src/DP/GoogleOaODDJOURNEY.java
@@ -6,35 +6,30 @@ import java.util.Scanner;
 public class GoogleOaODDJOURNEY {
 //    Find the number of journeys where you start from index 1 and end at index ‘n’ and the sum of every journey should be odd..
 public static void main(String[] args) {
-   int[] ar = {0, 2, 3, 5, 8, 10};
+   int[] ar = {0, 2, 3, 5, 8, 10};  // 1 index array
     System.out.println(countOddJourneys(ar));
 
 }
 
     public static int countOddJourneys(int[] arr) {
-        int[][] dp = new int[arr.length + 1][arr.length + 1];
-        if (arr[1] % 2 == 0) {      //even
-            dp[1][0] = 1;       //even + even = even
-            dp[1][1] = 0;       //even + odd = odd
+        int n = arr.length;
+        int[][] dp = new int[n + 1][2];
+
+        if (arr[1] % 2 == 0) {
+            dp[1][0] = 1;
         } else {
-            dp[1][0] = 0;       //odd + even = odd
-            dp[1][1] = 1;       //odd + odd = even
+            dp[1][1] = 1;
         }
 
-        int i = 2;
-        while (i < arr.length)
-        {
-            if (arr[i] % 2 != 0) {
-                // curr is odd :
-                dp[i][1] = dp[i - 1][0] + dp[i -2][0];
-                dp[i][0] = dp[i - 1][1] + dp[i - 2][1];
+        for (int i = 2; i < n; i++) {
+            if (arr[i] % 2 == 0) {
+                dp[i][0] = dp[i - 1][0] + dp[i - 2][0];     //even + even= even
+                dp[i][1] = dp[i - 1][1] + dp[i - 2][1];     //even + odd = odd
             } else {
-                //curr is even :
-                dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
-                dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+                dp[i][0] = dp[i - 1][1] + dp[i - 2][1];     //odd + odd = even
+                dp[i][1] = dp[i - 1][0] + dp[i - 2][0];     //odd + even = odd
             }
-            i++;
         }
-        return dp[arr.length - 1][1];
+        return dp[n - 1][1];
     }
 }
