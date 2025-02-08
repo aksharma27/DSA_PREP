46e35f7cd0ed68281b378176767290412b2fd0b1
Abhishek <aksharma.27mjm@gmail.com>
2025-02-08 13:39:43 +0530

    reduce n to 1 -1, /3, /5, /5 in min cost

--- src/DP/GoogleSDE3.java
+++ src/DP/GoogleSDE3.java
@@ -5,30 +5,30 @@ public class GoogleSDE3 {
 //    by 1 in y dollars, or divide it by  3 in x dollars, or div by 5 in z dollars, or divide by 7 in b dollars. Find the min way to reach 1.
 
     public static void main(String[] args) {
-        System.out.println(minWays(15, 100, 1, 50, 100000));
+        System.out.println(minWays(15, 100, 50, 85, 1));
     }
 
-    static int minWays (int n, int y, int x, int z, int b) {
+    static int minWays (int n, int y, int x, int z, int b) { //y -1, /3-x, /5-z, /7-b
         int[] dp = new int[n + 1];
-//        dp[0] = 0;
+//        int[]
         dp[1] = 0;
-        dp[2] = 1;
-        dp[3] = 1;
-        dp[4] = 2;
-
-        for (int i = 2; i <= n; i++) {
-            int a1 = dp[i-1] + y;
-            int a2 = Integer.MAX_VALUE;
-            int a3 = Integer.MAX_VALUE;
-            int a4 = Integer.MAX_VALUE;
-
-            if (i % 3 == 0)
-                a2 = dp[i / 3] + x;
-            if (i % 5 == 0)
-                a3 = dp[i / 5] + z;
-            if (i % 7 == 0)
-                a4 = dp[i / 7] + b;
-            dp[i] = Math.min(a1, Math.min (a4, Math.min(a3, a4)));
+        int i = 2;
+        while (i <= n) {
+            int v1 = dp[i - 1] + y;
+            int v2 = Integer.MAX_VALUE;
+            int v3 = Integer.MAX_VALUE;
+            int v4 = Integer.MAX_VALUE;
+            if (i % 3 == 0) {
+                v2 = dp[i / 3] + x;
+            }
+            if (i % 5 == 0) {
+                v3 = dp[i / 5] + z;
+            }
+            if (i % 7 == 0) {
+                v4 = dp[i / 7] + b;
+            }
+            dp[i] = Math.min(v1, Math.min(v2, Math.min(v3, v4)));
+            i++;
         }
         return dp[n];
     }
